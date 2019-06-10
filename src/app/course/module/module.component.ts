38d7f5857b2b3036f6625371f8d4e70f97cf6484
codingisllife <42679692+codingisllife@users.noreply.github.com>
2019-06-10 11:17:20 +0530

    added next-previous button

--- src/app/course/module/module.component.ts
+++ src/app/course/module/module.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from '../course.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Courses, CourseChapter, ChapterModule } from '../course';
 
 
@@ -10,39 +10,79 @@ import { Courses, CourseChapter, ChapterModule } from '../course';
 export class ModuleComponent implements OnInit {
 
     constructor(private courseService: CourseService,
-        private route: ActivatedRoute) { }
+        private route: ActivatedRoute,
+        private router:Router) { }
 
         course:Courses;
         chapters:CourseChapter[]
         modules:ChapterModule[];
         module:ChapterModule;
         count=1;
+        courseId:number;
+        chapterId:number;
+        moduleId:number;
+
+        array: Array<String> = [];
+        value:string;
+        length=0;
+
      ngOnInit(): void {
   
-this.route.paramMap.subscribe((map) =>{
-   let chapterId = Number(map.get("chapterId"));
-   let moduleId = Number(map.get("moduleId"));
+    this.route.paramMap.subscribe((map) =>{
+   this.chapterId = Number(map.get("chapterId"));
+   this.moduleId = Number(map.get("moduleId"));
 
-   console.log(chapterId+""+moduleId)
+   console.log(this.chapterId+""+this.moduleId)
    this.courseService.getCourseById().subscribe((data) =>{
        this.course = data;
+       this.courseId = this.course.courseId;
         this.chapters = this.course.courseChapter;
         
         let count = 1;  
-       for(let i =0;i<this.chapters.length;i++){
-           this.modules = this.chapters[0].chapterModule;
-            for(let j = 0;j<this.modules.length;j++){
+        
+        for(let i =0;i<this.chapters.length;i++){
+           this.modules = this.chapters[i].chapterModule;
+           for(let j = 0;j<this.modules.length;j++){
                 count++;
-            }
+                this.value = i + "" + j; 
+
+                if(this.length == 0){
+                    this.array.push(i + "" + j);
+                }
+
+           }
         }
+        this.length = this.array.length;
+        console.log(this.array)
         console.log(this.course)
-        this.module = this.chapters[chapterId].chapterModule[moduleId];
+        this.module = this.chapters[this.chapterId].chapterModule[this.moduleId];
         console.log(this.module)
 
    })
 })
 
-
-
     }
+
+    next(){
+        let value = this.chapterId +"" + this.moduleId; 
+        for(let i=0;i< this.array.length;i++){
+            if(value == this.array[i] && i!=this.array.length-1){
+                let got = this.array[i+1];
+                console.log(got)
+                this.router.navigate(["chapters/"+ this.courseId+"/modules/" + Number(got.charAt(0)) +"/" + Number(got.charAt(1))]);
+            }    
+        }
+    }
+
+    previous(){
+        let value = this.chapterId +"" + this.moduleId; 
+        for(let i=this.array.length-1;i>=0 ;i--){
+            if(value == this.array[i] && i!=0){
+                let got = this.array[i-1];
+                console.log(got)
+                this.router.navigate(["chapters/"+ this.courseId+"/modules/" + Number(got.charAt(0)) +"/" + Number(got.charAt(1))]);
+            }    
+        }
+    }
+
 }
